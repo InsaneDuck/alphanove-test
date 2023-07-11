c4770c34afaacad86b05336fe777d28da426860b
InsaneDuck <39309626+InsaneDuck@users.noreply.github.com>
2023-07-11 10:32:33 +0100

    updated

---
+++ src/main/java/dev/insaneduck/alphanovetest/config/RestConfiguration.java
@@ -0,0 +1,14 @@
+package dev.insaneduck.alphanovetest.config;
+
+import dev.insaneduck.alphanovetest.entites.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+@Configuration
+public class RestConfiguration implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Book.class);
+    }
+}
